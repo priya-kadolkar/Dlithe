18691777cac3ac8cfaf391848a391ac06c091b2d
priya-kadolkar <priyakadolkar95@gmail.com>
2022-04-16 23:57:13 +0530

    component creation

---
+++ Dlithe/Front-end/dl-task025-Angular-comp-creation-01-04-2022/angular_project3/heroes/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeroListComponent } from './hero-list/hero-list.component';
+import { HeroDetailsComponent } from './hero-details/hero-details.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroListComponent,
+    HeroDetailsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
